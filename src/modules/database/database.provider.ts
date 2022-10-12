415a10041a5d60cc3f0a92fe3dd85e65c33986f8
rehman.whala <rehman@renesistech.com>
2022-10-13 00:56:44 +0500

    db change

--- src/modules/database/database.provider.ts
+++ src/modules/database/database.provider.ts
@@ -1,12 +1,13 @@
 import * as mongoose from 'mongoose';
 import { env } from 'process';
+console.log(env.DATABASE_URL,"env.DATABASE_URL");
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>{ 
       return mongoose.connect(
-        env.DATABASE_URL
+        "mongodb+srv://alpharesearchtools:Burkan123@alpharesearchtools.hzt73.mongodb.net/art?retryWrites=true&w=majority"
       )}
   },
 ];
