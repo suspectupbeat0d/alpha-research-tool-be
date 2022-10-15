0315da9b6553114d94baab46dd592ac4e8b77c91
rehman.whala <rehman@renesistech.com>
2022-10-15 22:24:54 +0500

    db change

--- src/modules/database/database.provider.ts
+++ src/modules/database/database.provider.ts
@@ -7,7 +7,8 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>{ 
       return mongoose.connect(
-        "mongodb+srv://alpharesearchtools:Burkan123@alpharesearchtools.hzt73.mongodb.net/art?retryWrites=true&w=majority"
+        "mongodb://adminart:burkan123@194.163.142.38:30000/?directConnection=true&authSource=admin&appName=mongosh+1.5.4"
+        // "mongodb+srv://alpharesearchtools:Burkan123@alpharesearchtools.hzt73.mongodb.net/art?retryWrites=true&w=majority"
       )}
   },
 ];
